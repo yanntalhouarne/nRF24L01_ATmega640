7cfca22b8f52fa6c676f8c7f6492f54a43709ffa
Yann-motocrane <yt@motocrane.com>
2018-04-15 22:14:49 -0500

    f

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
  * Author : Yann
  */ 
 #define F_CPU 16000000
-#define LOOP_DELAY 30
+#define LOOP_DELAY 10
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -20,17 +20,24 @@
 
 #define BUFFER_SIZE 2
 
+void setup_TMR1();
+void reset_TMR1();
+
 char buffer[mirf_PAYLOAD] = {0,0};
 int mtr_cmd = 0;
 int srv_cmd = 0;
 
-uint8_t status = 0;
+uint8_t comm_lost_count = 0;
 int8_t tx_address[5] = {0xE7,0xE7,0xE7,0xE7,0xE7};
 int8_t rx_address[5] = {0xD7,0xD7,0xD7,0xD7,0xD7};
+	
+uint8_t status = 0;
+uint8_t comm_lost = 0;
 
 int main(void)
 {
 	setup_ports();
+	setup_TMR1();
 	setup_lcd();
 	setup_adc();
 	_delay_ms(10);
@@ -46,31 +53,55 @@ int main(void)
 	
 	sei(); // enable global interrupts
 	
-	//mirf_config_register(EN_AA, (1<<0));
-	//	mirf_config_register(EN_AA, (1<<1));
-	
 	mirf_config();
 	
 	 /* Set the device addresses */
 	mirf_set_TADDR(tx_address);
 	mirf_set_RADDR(rx_address);
 	
-	
 	_delay_ms(10);
 	
 	print_0("System initialized...;");
+	
+	lcd_send_cmd(CLEAR_DISPLAY);
+	_delay_ms(3);
+
+	lcd_set_cursor(1,1);
+	lcd_print("mtr:  ");
+	
+	lcd_set_cursor(2,1);
+	lcd_print("srv: ");
+	lcd_print_int(mtr_cmd);
+	
+	lcd_set_cursor(1,9);
+	lcd_print("RF: ");
+	lcd_print_int(mtr_cmd);
 
     while (1) 
     {
-		// to UART
-		//println_0("Waiting for data...;");
-		// to LCD
-		//lcd_send_cmd(CLEAR_DISPLAY);
-		//_delay_ms(3);
-		//lcd_set_cursor(1,1);
-		//lcd_print("Waiting for data");
-		//_delay_ms(3);
 		
+		TOGGLE_LED1;
+		lcd_set_cursor(1,5);
+		lcd_print_int(mtr_cmd);
+		lcd_print(" ");
+		lcd_set_cursor(2,5);
+		lcd_print_int(srv_cmd);
+		lcd_print(" ");
+		if (comm_lost_count == 0)
+		{
+			lcd_set_cursor(1,13);
+			lcd_print("GOOD");
+		}
+		else if (((comm_lost_count > 6) && (comm_lost_count < 15)))
+		{
+			lcd_set_cursor(1,13);
+			lcd_print("BAD  ");
+		}
+		else if (comm_lost_count > 15)
+		{
+			lcd_set_cursor(1,13);
+			lcd_print("LOST");
+		}
 		mtr_cmd = analog_get_average(POT1, 5);
 		mtr_cmd /= ADC_SCALING;
 		buffer[0] = mtr_cmd;
@@ -82,77 +113,40 @@ int main(void)
 			srv_cmd = 45;
 		buffer[1] = srv_cmd;
 		
-		//println_int_0(mtr_cmd);
-		
-		TOGGLE_LED1;
 		mirf_send(buffer, mirf_PAYLOAD);
 		_delay_us(10);
-		while (!mirf_data_sent());
-		TOGGLE_LED1
-		//mirf_config_register(STATUS, (1 << TX_DS) | (1 << MAX_RT)); // Reset status register
-		//lcd_send_cmd(CLEAR_DISPLAY);
-		//mirf_config_register(STATUS, (1 << RX_DR) | (1 << MAX_RT)); // Reset status register
-		//LED1_ON; // turn on LED when data is received
+		reset_TMR1();
+		//TOGGLE_LED6;
 		
-		//mirf_get_data(buffer);
-		
-		//println_int_0(buffer[0]);
-		//lcd_send_cmd(CLEAR_DISPLAY);
-		//lcd_set_cursor(1,1);
-		//lcd_print_int(buffer[0]);
-		
-		
-		/*
-		// send to UART
-		print_0("Data received: ;");
-		print_char_0(buffer[0]);
-		print_char_0(',');
-		print_char_0(' ');
-		print_char_0(buffer[1]);
-		print_char_0(NL);
-		*/
-		
-		// send to LCD
-		//lcd_send_cmd(CLEAR_DISPLAY);
-		//_delay_ms(3);
-		//lcd_set_cursor(1,1);
-		//lcd_print("Data received:");
-		//_delay_ms(3);
-		//lcd_set_cursor(2,1);
-		//lcd_print_int(buffer[0]);
-		//_delay_ms(3);
-		//lcd_print(", ");
-		//_delay_ms(3);
-		//lcd_print_int(buffer[1]);
-		//_delay_ms(3);
-		
-	//	_delay_ms(5000);
-		
-		//buffer[0] = 'K';
-		
-		//lcd_send_cmd(CLEAR_DISPLAY);
-		//_delay_ms(3);
-		//lcd_set_cursor(1,1);
-		//_delay_ms(3);
-		//lcd_print("Sending data...");
-		//_delay_us(10);
-		//_delay_us(10);
-		//lcd_send_cmd(CLEAR_DISPLAY);
-		///lcd_set_cursor(1,1);
-		//lcd_print("waiting on TX");
-		//mirf_send(buffer, mirf_PAYLOAD);
-		//while (!mirf_data_sent());
-		//lcd_send_cmd(CLEAR_DISPLAY);
-		//mirf_config_register(STATUS, (1 << TX_DS) | (1 << MAX_RT)); // Reset status register
-		//TOGGLE_LED1;
-		//lcd_set_cursor(2,1);
-		//_delay_ms(3);
-		//lcd_print("Data sent.");
-		
-		//_delay_ms(500);
-		
-		//while(1);
+		while (!mirf_data_sent())
+		{
+			if (TCNT1 > 62499) // timeout of one second
+			{
+				comm_lost_count++;
+				comm_lost = 1;
+				TOGGLE_LED6;
+				break;
+			}
+		}
+		if (!comm_lost) // a successful transaction decrements comm_lost_count while a time-out increments it
+		{
+			comm_lost_count--; // decrement comm_lost_count
+			if (comm_lost_count < 0) // prevent comm_lost_count from being negative
+				comm_lost_count = 0;
+		}
+		comm_lost = 0;
 		
 		_delay_ms(LOOP_DELAY);
     }
+}
+
+void setup_TMR1()
+{
+	TCCR1B |= (1<<CS12); // 256 prescaler, CTC mode
+
+}
+
+void reset_TMR1()
+{
+	TCNT1 = 0;
 }
